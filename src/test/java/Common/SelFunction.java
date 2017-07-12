32f630574120e50186d00b4d82fe3832363a77c6
singh143sonu <sandeep.bhadouria@gmail.com>
2017-07-12 20:49:24 +1200

    url changed
    google.com

--- src/test/java/Common/SelFunction.java
+++ src/test/java/Common/SelFunction.java
@@ -15,7 +15,7 @@ public class SelFunction {
         public static WebDriver driver = null;
         public static WebDriverWait waitVar = null;
 
-        public static String baseURL = "https://github.com/";
+        public static String baseURL = "https://google.com/";
 
         public void createDriver() throws MalformedURLException, InterruptedException {
             System.setProperty("webdriver.gecko.driver", "C:/Users/Jyoysna/Downloads/geckodriver-v0.18.0-win64/geckodriver.exe");
